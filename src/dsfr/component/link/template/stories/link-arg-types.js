7c50e1b8ec0af221d569f3fafcb0822d43161e0f
Keryan SANIE <keryan.sanie@gmail.com>
2026-06-09 17:22:28 +0200

    ✨ feat(link): ajoute la variation lien au fil du texte (#1455)
    - Ajoute un paramètre inText permettant de retirer le style du lien

--- src/dsfr/component/link/template/stories/link-arg-types.js
+++ src/dsfr/component/link/template/stories/link-arg-types.js
@@ -51,9 +51,13 @@ const linkArgTypes = {
     control: 'boolean',
     description: 'Le lien est désactivé'
   },
+  inText: {
+    control: 'boolean',
+    description: 'Si true, le lien est intégré dans un texte et la classe `fr-link` n’est pas ajoutée'
+  },
   blank: {
     control: 'boolean',
-    description: 'Si true, target prend la valeur _blank, sinon _self'
+    description: 'Si true, target prend la valeur `_blank`, sinon `_self`'
   },
   title: {
     if: { arg: 'blank', eq: true },
@@ -103,6 +107,7 @@ const linkArgs = {
   label: 'libellé du lien',
   size: 'md',
   disabled: false,
+  inText: false,
   download: false,
   detail: '',
   hreflang: '',
@@ -122,6 +127,7 @@ const linkProps = (args) => {
     size: args.size || linkArgs.size,
     id: args.id || undefined,
     disabled: args.disabled,
+    inText: args.inText,
     href: args.href || linkArgs.href,
     download: args.download || linkArgs.download,
     detail: args.detail || linkArgs.detail,
